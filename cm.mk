2672c7c573a69700db3afcd1609509068faff7bb
Endre <ecseko@gmail.com>
2016-09-11 21:09:50 -0400

    Further tweaks for N

--- cm.mk
+++ cm.mk
@@ -15,6 +15,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from serranodsdd device
+$(call inherit-product, device/samsung/serranodsdd/device.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
